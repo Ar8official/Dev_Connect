eb540291fe8c0acfa1c0579244e029a568635ad5
Borguuh <abdrahmanadewale@gmail.com>
2021-01-13 23:26:41 +0100

    post release mods

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,18 +17,17 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Connector</h1>
+                <h1 className="display-3 mb-4">Social Developers</h1>
                 <p className="lead">
                   {' '}
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
+                  Join today to create your own developer profile, create posts and meet other social developers.
                 </p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
-                  Sign Up
+                  Register
                 </Link>
                 <Link to="/login" className="btn btn-lg btn-light">
-                  Login
+                  Sign in
                 </Link>
               </div>
             </div>
